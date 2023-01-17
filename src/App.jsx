08631f375aa4d0527247cfcd48687533b665224c
Edwin A. Espinal Taveras <espinaledwin1919@gmail.com>
2023-01-17 16:11:41 -0400

    Cambio a React

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import reactLogo from "./assets/meesage.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <h1 className='text-9xl text-lime-500 underline font-extrabold'>
+      Hello World
+    </h1>
+  );
+}
+
+export default App;
